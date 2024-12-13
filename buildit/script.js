b27aed9c19e059f3aed1cc1c082cc6a576525744
Aryan Pandey <134038788+ar-yan-pandey@users.noreply.github.com>
2024-12-14 02:20:40 +0530

    Add files via upload

---
+++ buildit/script.js
@@ -0,0 +1,8 @@
+document.getElementById('assesment').addEventListener('click', function () {
+    document.getElementById('fullscreenPopup').style.display = 'flex';
+  });
+  
+  document.getElementById('closePopup').addEventListener('click', function () {
+    document.getElementById('fullscreenPopup').style.display = 'none';
+  });
+  
